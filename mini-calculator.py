56d9cf347422c08bb65fe43fa5dbb9958c84aec3
h0len <manuylov.alex9311@gmail.com>
2024-09-30 03:09:45 +0300

    mini-calculator.py

--- mini-calculator.py
+++ mini-calculator.py
@@ -4,11 +4,15 @@ def calculate(first_number, operationn, second_number):
     """Функция, принимающая три значения: первое число, операцию и второе число. Она определяет, какую операцию нужно сделать и выполняет ее."""
     try:
         if operationn == "плюс":
-            return determinator_of_number(first_number) + determinator_of_number(second_number)
+            return undo_helper_of_determinator((determinator_of_number(first_number)) + determinator_of_number(second_number))
         elif operationn == "минус":
-            return determinator_of_number(first_number) - determinator_of_number(second_number)
+            return undo_helper_of_determinator((determinator_of_number(first_number)) - determinator_of_number(second_number))
+        #ДЕЛИТЬ НЕ РАБОТАЕТ
+        elif operationn == "разделить":
+            return undo_helper_of_determinator((determinator_of_number(first_number)) / determinator_of_number(second_number))
+
         else:
-            return determinator_of_number(first_number) * determinator_of_number(second_number)
+            return undo_helper_of_determinator((determinator_of_number(first_number)) * determinator_of_number(second_number))
     except TypeError:
         print("Изначально были неправильно введены значения!")
         sys.exit(0)
@@ -27,7 +31,9 @@ def from_list_to_integer(list):
 
 def helper_of_determinator(listt):
     """Функция для составления списка из одного элемента. В данной программе, она возвращает число(первое или второе), которое было введено пользователем."""
-    return list(filter(None, [numbers_from_0_to_9.get(from_list_to_string(listt)), numbers_from_10_to_19.get(from_list_to_string(listt)), numbers_mod10_equal_zero.get(from_list_to_string(listt))]))
+    return list(filter(None, [numbers_from_0_to_9.get(from_list_to_string(listt)),
+                              numbers_from_10_to_19.get(from_list_to_string(listt)),
+                              numbers_mod10_equal_zero.get(from_list_to_string(listt))]))
 
 def determinator_of_operation(list):
     """Функция для определения операции, и возврата ее."""
@@ -40,7 +46,10 @@ def determinator_of_operation(list):
 def determinator_of_number(number):     #Принимает список
     """Функция для перевода числа из списка в целочисленный формат."""
     if len(number) == 1:
-        return (from_list_to_integer(helper_of_determinator(number)))
+        if from_list_to_integer(helper_of_determinator(number)) == 100:
+            return 0
+        else:
+            return (from_list_to_integer(helper_of_determinator(number)))
     else:
         if "минус" in number:
             number.remove("минус")
@@ -54,8 +63,23 @@ def determinator_of_number(number):     #Принимает список
             number_split_first_part, number_split_second_part = [str(word) for word in number]
             return (from_list_to_integer(helper_of_determinator(number_split_first_part)) + from_list_to_integer(helper_of_determinator(number_split_second_part)))
 
+def get_key_by_value(dictionary, value):
+    for key, v in dictionary.items():
+        if v == value:
+            return key
+    return None
+
+def undo_helper_of_determinator(integer):
+    """Функция для перевода числа из целочисленного формата в текстовый."""
+    if 1 <= integer < 20:
+        return " ".join(list(filter(None, [get_key_by_value(numbers_from_0_to_9, integer),
+                                  get_key_by_value(numbers_from_10_to_19, integer)])))
+    else:
+        return " ".join(list(filter(None, [get_key_by_value(numbers_mod10_equal_zero, (integer - (integer % 10))),
+                                  get_key_by_value(numbers_from_0_to_9, integer % 10),])))
+
 #Как вообще задавать число?
-numbers_from_0_to_9 = {"ноль":0, "один":1, "два":2,
+numbers_from_0_to_9 = {"ноль":100, "один":1, "два":2,
                         "три":3, "четыре":4, "пять":5,
                         "шесть":6, "семь":7, "восемь":8,
                         "девять":9}
@@ -73,30 +97,36 @@ numbers_used_in_float = {"одна":1, "две":2, "три":3,
                          "четыре":4, "пять":5, "шесть":6,
                          "семь":7, "восемь":8, "девять":9}          #При делении нужно
 
-operations = ["плюс", "минус", "умножить"]
+operations = ["плюс", "минус", "умножить", "разделить"]
 
 print("Добро пожаловать в калькулятор! Введите выражение в формате: {первое число} {операция} {второе число}\nПример: \"двадцать три минус девятнадцать\"")
 flag = False
 while flag == False:
     expression = input().lower()
     words = list(filter(None, expression.split(" ")))
-    if (words.count("плюс") + words.count("минус") + words.count("умножить") == 1):
+    if (words.count("плюс") + words.count("минус") + words.count("умножить") + words.count("разделить") == 1):
         operation = determinator_of_operation(words)
         index_of_operation = words.index(operation)
         first_num = words[:index_of_operation]
         second_num = words[(index_of_operation + 1):]
         if ((2 <= len(first_num) + len(second_num) <= 4) and (operation is not None) and (len(first_num) != 0) and (len(second_num) != 0)):
             flag = True
-    elif (words.count("плюс") + words.count("минус") + words.count("умножить") == 2) and (words.count("минус") > 0):
+            if ((operation == "разделить") and (from_list_to_string(second_num) == "ноль")):
+                flag = False
+                print("Делить на ноль нельзя! Введите запрос корректно: ")
+    elif (words.count("плюс") + words.count("минус") + words.count("умножить") + words.count("разделить") == 2) and (words.count("минус") > 0):
         operation = determinator_of_operation(words)
         index_of_operation = words.index(operation)
         first_num = words[:index_of_operation]
         second_num = words[(index_of_operation + 1):]
         if ((3 <= len(first_num) + len(second_num) <= 5) and (operation is not None) and (len(first_num) != 0) and (len(second_num) != 0)):
             flag = True
+            if ((operation == "разделить") and (from_list_to_string(second_num) == "ноль")):
+                flag = False
+                print("Делить на ноль нельзя! Введите запрос корректно: ")
     else:
         print("Неправильный формат ввода! Введите еще раз: ")
 
 print(f"Ваш результат: {calculate(first_num, operation, second_num)}")
 
-#ДОП ЗАДАНИЯ - 5
+#ДОП ЗАДАНИЯ - 5, 1(пока что нет)
