526c2325767a2793f913fcc257e389e8a4bb2921
h0len <manuylov.alex9311@gmail.com>
2024-10-03 00:12:57 +0300

    mini-calculator.py

--- mini-calculator.py
+++ mini-calculator.py
@@ -86,7 +86,10 @@ def undo_helper_of_determinator_for_integers_until_1000(integer):
     if 1 <= integer < 20:
         return " ".join(list(filter(None, [get_key_by_value(numbers_from_0_to_9, integer), get_key_by_value(numbers_from_10_to_19, integer)])))
     if 20 <= integer < 100:
-        return " ".join(list(filter(None, [get_key_by_value(numbers_mod10_equal_zero, (integer - (integer % 10))), get_key_by_value(numbers_from_0_to_9, (integer % 10))])))
+        if integer % 10 == 0:
+            return " ".join(list(filter(None, [get_key_by_value(numbers_mod10_equal_zero, (integer - (integer % 10)))])))
+        else:
+            return " ".join(list(filter(None, [get_key_by_value(numbers_mod10_equal_zero, (integer - (integer % 10))), get_key_by_value(numbers_from_0_to_9, (integer % 10))])))
     if 100 <= integer < 1000:
         if len(list(filter(None, [get_key_by_value(numbers_mod100_equal_zero, int(str(integer)[0])), get_key_by_value(numbers_mod10_equal_zero, ((integer % 100) - (integer % 10))), get_key_by_value(numbers_from_0_to_9, int(str(integer)[2]))]))) > 1 and (integer % 10 == 0):
             return " ".join(list(filter(None, [get_key_by_value(numbers_mod100_equal_zero, int(str(integer)[0])), get_key_by_value(numbers_mod10_equal_zero, (integer % 100) - (integer % 10))])))
@@ -111,12 +114,19 @@ def undo_helper_of_determinator(integer_or_float):
             result +=  " ".join(list(filter(None, [get_key_by_value(numbers_from_0_to_9, integer_part),
                                                    get_key_by_value(numbers_from_10_to_19, integer_part)])))
         elif 20 <= integer_part < 100:
-            result += " ".join(list(filter(None, [get_key_by_value(numbers_mod10_equal_zero, (integer_part - (integer_part % 10))),
-                                                  get_key_by_value(numbers_from_0_to_9, integer_part % 10)])))
+            if integer_part % 10 == 0:
+                result += " ".join(list(filter(None, [get_key_by_value(numbers_mod10_equal_zero, (integer_part - (integer_part % 10)))])))
+            else:
+                result += " ".join(list(filter(None, [get_key_by_value(numbers_mod10_equal_zero, (integer_part - (integer_part % 10))),
+                                                      get_key_by_value(numbers_from_0_to_9, integer_part % 10)])))
         elif 100 <= integer_part < 1000:
-            result += " ".join(list(filter(None, [get_key_by_value(numbers_mod100_equal_zero, int(str(integer_part)[0])),
-                                                  get_key_by_value(numbers_mod10_equal_zero, ((integer_part % 100) - (integer_part % 10))),
-                                                  get_key_by_value(numbers_from_0_to_9, (integer_part % 10))])))
+            if integer_part % 10 == 0:
+                result += " ".join(list(filter(None, [get_key_by_value(numbers_mod100_equal_zero, int(str(integer_part)[0])),
+                                                      get_key_by_value(numbers_mod10_equal_zero, ((integer_part % 100) - (integer_part % 10)))])))
+            else:
+                result += " ".join(list(filter(None, [get_key_by_value(numbers_mod100_equal_zero, int(str(integer_part)[0])),
+                                                      get_key_by_value(numbers_mod10_equal_zero, ((integer_part % 100) - (integer_part % 10))),
+                                                      get_key_by_value(numbers_from_0_to_9, (integer_part % 10))])))
         elif 1000 <= integer_part < 10000:
                 if (len(list(filter(None, [get_key_by_value(numbers_used_in_thousands, int(str(integer_part)[0])), get_key_by_value(numbers_mod100_equal_zero, int(str(integer_part)[1])), get_key_by_value(numbers_mod10_equal_zero, int(str(integer_part)[1:]) - integer_part % 10), get_key_by_value(numbers_from_0_to_9, int(str(integer_part)[3]))]))) > 1) and (integer_part % 10 == 0):
                     result +=  " ".join(list(filter(None, [get_key_by_value(numbers_to_thousands, int(str(integer_part)[0])),
@@ -145,31 +155,31 @@ def undo_helper_of_determinator(integer_or_float):
 #минус число операция минус число, числа двузначные
 
 #Как вообще задавать число?
-numbers_from_0_to_9 = {"ноль":0,"один":1, "два":2,
-                        "три":3, "четыре":4, "пять":5,
-                        "шесть":6, "семь":7, "восемь":8,
-                        "девять":9}
+numbers_from_0_to_9 = {"ноль": 0,"один": 1, "два": 2,
+                        "три": 3, "четыре": 4, "пять": 5,
+                        "шесть": 6, "семь": 7, "восемь": 8,
+                        "девять": 9}
 
-numbers_from_10_to_19 = {"десять":10, "одиннадцать":11, "двенадцать":12,
-                         "тринадцать":13,"четырнадцать":14, "пятнадцать":15,
-                         "шестнадцать":16,"семнадцать":17, "восемнадцать":18,
-                         "девятнадцать":19}
+numbers_from_10_to_19 = {"десять": 10, "одиннадцать": 11, "двенадцать": 12,
+                         "тринадцать": 13,"четырнадцать": 14, "пятнадцать": 15,
+                         "шестнадцать": 16,"семнадцать": 17, "восемнадцать": 18,
+                         "девятнадцать": 19}
 
-numbers_mod10_equal_zero = {"двадцать":20, "тридцать":30, "сорок":40,
-                            "пятьдесят":50,"шестьдесят":60, "семьдесят":70,
-                            "восемьдесят":80, "девяносто":90}
+numbers_mod10_equal_zero = {"двадцать": 20, "тридцать": 30, "сорок": 40,
+                            "пятьдесят": 50,"шестьдесят": 60, "семьдесят": 70,
+                            "восемьдесят": 80, "девяносто": 90}
 
-numbers_used_in_thousands = {"ноль":0, "одна":1, "две":2, "три":3,
-                         "четыре":4, "пять":5, "шесть":6,
-                         "семь":7, "восемь":8, "девять":9}
+numbers_used_in_thousands = {"ноль": 0, "одна": 1, "две": 2, "три": 3,
+                         "четыре": 4, "пять": 5, "шесть": 6,
+                         "семь": 7, "восемь": 8, "девять": 9}
 
-numbers_mod100_equal_zero = {"сто":1, "двести":2, "триста":3,
-                             "четыреста":4, "пятьсот":5, "шестьсот":6,
-                             "семьсот":7, "восемьсот":8, "девятьсот":9}
+numbers_mod100_equal_zero = {"сто": 1, "двести": 2, "триста": 3,
+                             "четыреста": 4, "пятьсот": 5, "шестьсот": 6,
+                             "семьсот": 7, "восемьсот": 8, "девятьсот": 9}
 
-numbers_to_thousands = {"одна тысяча":1, "две тысячи":2, "три тысячи":3,
-                        "четыре тысячи":4, "пять тысяч":5, "шесть тысяч":6,
-                        "семь тысяч":7, "восемь тысяч":8, "девять тысяч":9}
+numbers_to_thousands = {"одна тысяча": 1, "две тысячи": 2, "три тысячи": 3,
+                        "четыре тысячи": 4, "пять тысяч": 5, "шесть тысяч": 6,
+                        "семь тысяч": 7, "восемь тысяч": 8, "девять тысяч": 9}
 
 operations = ["плюс", "минус", "умножить", "разделить"]
 
@@ -201,9 +211,9 @@ while flag == False:
             index_of_operation = words.index(operation)
             first_num = words[:index_of_operation]
             second_num = words[(index_of_operation + 1):]
-        if ((3 <= len(first_num) + len(second_num) <= 6) and (operation is not None) and (0 < len(first_num) < 4) and (0 < len(second_num) < 4)):
+        if (3 <= len(first_num) + len(second_num) <= 6) and (operation is not None) and (0 < len(first_num) < 4) and (0 < len(second_num) < 4):
             flag = True
-            if ((operation == "разделить") and (from_list_to_string(second_num) == "ноль")):
+            if (operation == "разделить") and (from_list_to_string(second_num) == "ноль"):
                 flag = False
                 print("Делить на ноль нельзя! Введите запрос корректно: ")
         else:
